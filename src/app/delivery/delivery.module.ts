bd4399f211b7a7c101ea28f3992d51f4e72d9965
Elias Michalczuk <eliasmichalczuk@gmail.com>
2019-03-20 20:56:01 -0300

    novos campos em meals component

--- src/app/delivery/delivery.module.ts
+++ src/app/delivery/delivery.module.ts
@@ -6,7 +6,9 @@ import { DeliveryRoutingModule } from './delivery-routing.module';
 import { MealsComponent } from './meals/meals.component';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { MatCardModule } from '@angular/material/card';
-
+import {MatSelectModule} from '@angular/material/select';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatIconModule} from '@angular/material/icon';
 @NgModule({
   declarations: [DeliveryComponent, MealsComponent],
   imports: [
@@ -14,7 +16,10 @@ import { MatCardModule } from '@angular/material/card';
     DeliveryRoutingModule,
     SharedModule,
     FlexLayoutModule,
-    MatCardModule
+    MatCardModule,
+    MatSelectModule,
+    MatFormFieldModule,
+    MatIconModule
   ]
 })
 export class DeliveryModule {}
